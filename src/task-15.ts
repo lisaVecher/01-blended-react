bb18c9829e143c4bf8833aa96d48f7ebed7ed69c
Yelyzaveta Vecherovska <vecheli0306@gmail.com>
2026-03-18 08:32:22 +0200

    Update task-15.ts

--- src/task-15.ts
+++ src/task-15.ts
@@ -22,5 +22,38 @@
 
 // Завдання:
 
+// Оголоси тип function fetchProducts() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve([
+//         { id: 1, title: "Laptop", price: 1000 },
+//         { id: 2, title: "Phone", price: 500 }
+//       ]);
+//     }, 1000);
+//   });
+// }
+
 // Оголоси тип Product для товару.
 // Додайте до функції явну типізацію, вкажіть, що вона повертає проміс, який приводиться до масиву товарів.
+
+
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+};
+
+function fetchProducts(): Promise<Product[]> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([
+        { id: 1, title: "Laptop", price: 1000 },
+        { id: 2, title: "Phone", price: 500 },
+      ]);
+    }, 1000);
+  });
+}
+
+fetchProducts().then((products: Product[]) => {
+  console.log(products);
+});
