d5f3d8d863bd2f6b5f20c3750248c3641b9f2471
Yelyzaveta Vecherovska <vecheli0306@gmail.com>
2026-03-18 08:25:58 +0200

    Update task-14.ts

--- src/task-14.ts
+++ src/task-14.ts
@@ -14,3 +14,14 @@
 
 // Додайте до функції явну типізацію, яка вказує, що вона повертає проміс який приводиться до рядка.
 // Переконайтеся, що якщо message має тип відмінний від рядка, то виникає помилка.
+
+
+function fetchMessage(): Promise<string> {
+  return new Promise((resolve) => {
+    resolve("Hello from server!");
+  });
+}
+
+fetchMessage().then((message: string) => {
+  console.log(message);
+});
